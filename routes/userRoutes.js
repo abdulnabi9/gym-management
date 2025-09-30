9083ba9e6476b2018890986681aaadc1c32e22b5
Nabi Abdul <nabiabdul943@gmail.com>
2025-09-30 17:52:53 +0530

    this is commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -154,6 +154,24 @@ router.get("/", auth, async (req, res) => {
 });
 
 
+router.get("/abdul",async(req, res)=>{
+
+try{
+  
+res.status(200).json({ "KRanthi": "Kranthi" });
+
+}catch(err){
+    res.status(500).json({ message: err.message });
+}
+
+})
+
+
+
+
+
+
+
 
 
 
